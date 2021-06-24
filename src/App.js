fbe5908d9f0bb2332f810cbd2995422a8b39c615
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-06-23 21:44:29 -0400

    Constructor and router
    constructor implementation and routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// src/components/App.js
+
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
+    }
+  }
+
   render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-        </Switch>
-      </Router>
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+          </div>
+        </Router>
     );
   }
-}
 
-/*
-constructor() {
-  super();
-
-  this.state = {
-    accountBalance: 14568.27
-  }
 }
-*/
 
 export default App;
-
