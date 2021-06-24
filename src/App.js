2971905c93f90233faa75ce50710dee6052b242a
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-06-23 21:28:31 -0400

    router implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello World
-        </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
