86a5ca388bd82861a4496fe5608cf3fec9b5a336
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-06-26 17:17:38 -0400

    Programmatic Redirect
    mock sign in component that redirects after a form is submitted

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ class App extends Component {
     }
   }
 
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
+
   render() {
 
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
@@ -26,11 +33,15 @@ class App extends Component {
         <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
     );
 
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
+
+
     return (
         <Router>
           <div>
             <Route exact path="/" render={HomeComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
           </div>
         </Router>
     );
