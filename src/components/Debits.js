14e9311975d9e590446e1a1df6a444a2fa05c1c3
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-07-01 18:46:54 -0400

    render and export Debits

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -47,6 +47,37 @@ class Debits extends Component {
     }
     
     
+  render() {
+    return (
+      <div>
+        <h1>Debits</h1>
+        <table className="data">
+          {this.state.debits.map(
+            debits =>
+              <tr key={debits.id}>
+                <td> Amount: {debits.amount}</td>
+                <td> Description: {debits.description}</td>
+                <td> Date: {debits.date}</td>
+              </tr>
+          )}
+        </table>
+        <br/>
+        <form>
+          <input type="text" value={this.state.description} onChange={this.handleDescription} placeholder="Enter Description"/>
+          <input type="number" value={this.state.amount} onChange={this.handleAmount} placeholder="Enter Amount"/>
+          <button className="add" onClick={this.addDebit}>Add Debit</button>
+        </form>
 
-
+        <AccountBalance accountBalance={this.props.accountBalance} />
+        <Link to="/login">Login</Link>
+        <Link to="/">Return to home</Link>
+        <Link to="/userprofile">User Profile</Link>
+        <Link to="/credits">Credits</Link>
+      </div>
+    );
+  }
 }
+
+export default Debits;
+
+
