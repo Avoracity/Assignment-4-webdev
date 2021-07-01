d4f2077aedb94affb2c3c70f61482eb451e5f68d
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-07-01 18:22:00 -0400

    class component and props

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import AccountBalance from './AccountBalance';
+
+
+
+class Debits extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        debits: []
+      }
+    }
