dc58c565ae329c6805fc72e0c937e1761cdbcf37
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-07-01 19:28:39 -0400

    Credits component and props

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -2,3 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import AccountBalance from './AccountBalance';
+
+
+class Credits extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        credits: []
+      }
+    }
+
+
+
+
+
+
+
+}
